8c730d4e343fa9b93a3a26dbf86061d48b534374
neko <1330040125@qq.com>
2021-03-10 10:45:39 +0800

    feat(init): curl封装

--- README.md
+++ README.md
@@ -3,5 +3,19 @@
 var_dump(math_gtz(-1)); // Math::GTZ(-1);
 // int(0)
 
+// curl的请求封装助手方法
+$info = c_get('https://baidu.com/',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+$info = c_post('https://baidu.com/',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+
+// 请求失败重试3次 300ms超时
+$obj = new \Neko\Libs\CUrl(3);
+$info = $obj->get('https://baidu.com/',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+// string(75) "https://baidu.com/?info=id%5C%2A%40+%24%23%21%2A%26%3E.&num=3&va=132.2222222"
+
+// 请求失败重试3次 300ms超时
+$obj = new \Neko\Libs\CUrl(3);
+$info = $obj->get('https://baidu.com/?api=aaaa',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+// string(84) "https://baidu.com/?api=aaaa&info=id%5C%2A%40+%24%23%21%2A%26%3E.&num=3&va=132.2222222"
+
 
 ```
--- src/helper.php
+++ src/helper.php
@@ -1,5 +1,5 @@
 <?php
-if (!function_exists('math_gtz')){
+if (!function_exists('math_gtz')) {
     /**
      * 返回一个绝对大于0的数小于0返回0
      *
@@ -7,7 +7,47 @@ if (!function_exists('math_gtz')){
      *
      * @return float|int|string
      */
-    function math_gtz($value){
+    function math_gtz($value)
+    {
         return \Neko\Libs\Math::GTZ($value);
+    }
+}
+
+if (!function_exists('c_get')) {
+    /**
+     * GET方式的请求
+     * c_get('https://baidu.com/',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+     *
+     * @param string $url       请求的链接
+     * @param array  $data      get参数
+     * @param int    $timeout   超时设置，单位：毫秒
+     * @param int    $retry_num 重试次数
+     *
+     * @return string  接口返回的内容，超时返回false
+     */
+    function c_get($url, $data = [], $timeout = 3000, $retry_num = 3)
+    {
+        // 请求失败重试3次 300ms超时
+        $obj = new \Neko\Libs\CUrl($retry_num);
+        return $obj->get($url, $data, 300);
+    }
+}
+if (!function_exists('c_post')) {
+    /**
+     * Post方式的请求
+     * c_post('https://baidu.com/',['info'=>'id\*@ $#!*&>.','num'=>3,'va'=>132.2222222],300);
+     *
+     * @param string $url       请求的链接
+     * @param array  $data      参数
+     * @param int    $timeout   超时设置，单位：毫秒
+     * @param int    $retry_num 重试次数
+     *
+     * @return string  接口返回的内容，超时返回false
+     */
+    function c_post($url, $data = [], $timeout = 3000, $retry_num = 3)
+    {
+        // 请求失败重试3次 300ms超时
+        $obj = new \Neko\Libs\CUrl($retry_num);
+        return $obj->post($url, $data, 300);
     }
 }
